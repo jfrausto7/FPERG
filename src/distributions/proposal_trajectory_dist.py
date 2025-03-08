851507d14e12e762c66ce28e56d287a8242c908c
yasabukh <yasabukh@users.noreply.github.com>
2025-03-08 14:47:33 -0800

    widened proposal initial state distribution

--- src/distributions/proposal_trajectory_dist.py
+++ src/distributions/proposal_trajectory_dist.py
@@ -12,15 +12,15 @@ class ProposalTrajectoryDistribution:
     Intitial state is from the reset() function in Grasp.env(). The only probabilistic component of the state
     is the x and y object position. 
     x = 0.5 + 0.05 * random.uniform(-1, 1) 
-    y = 0.0 + 0.05 * random.uniform(-1, 1)
+    y = 0.0 + 0.1 * random.uniform(-1, 1)
     Want to return a distribution of possible x and y values based on these equatinos. 
     Transforming the uniform distribution we get:
-    x ~ uniform(0.45, 0.1) and y ~ uniform(-0.05, 0.1) (in form of  uniform(mean, scale))
+    x ~ uniform(0.45, 0.1) and y ~ uniform(-0.1, 0.2) (in form of  uniform(loc, scale))
     We return a distribution for the x and y object position 
     """
     def initial_state_distribution(self):
         # I could add disturbance to the initial state to make failures more likely
-        return [uniform(0.45, 0.1), uniform(-0.05, 0.1)]
+        return [uniform(0.45, 0.1), uniform(-0.1, 0.2)]
 
 
     """ We add a disturbance only on the system's sensor. 
